fb8eed89cf341761efca7e52afc4d7577db2bc81
Jonathan Jupke <jonathanfrederik@aol.com>
2021-09-27 12:20:33 +0200

    fix ct_prepare_data_1

--- R/ct_prepare_data_1.R
+++ R/ct_prepare_data_1.R
@@ -11,31 +11,39 @@
 #'
 ct_prepare_data1 <- function(data, taxon, abundance.to.pa = TRUE, typologies) {
 
+        #- Check inputs
+        if (!"data.table" %in% class(data)) stop("data must be a data.table")
+        if (!"character" %in% class(taxon)) stop("taxon must be a character")
+        if (!"character" %in% class(typologies)) stop("typologies must be a character vector")
+
         #- Character vector with column names to keep
         col.vec <- append(c("gr_sample_id", taxon, "abundance"), typologies)
         #- Subset data to columns from col.vec
         x1 <- data[, .SD, .SDcols = col.vec]
         #- remove any NA in taxon column
-        if ("species" %in% names(x1)) x1 <- x1[!is.na(species)]
-        if ("genus"   %in% names(x1)) x1 <- x1[!is.na(genus)]
-        if ("family"  %in% names(x1)) x1 <- x1[!is.na(family)]
+        if ("species" %in% names(x1)) {
+                x1 <- x1[!is.na(species)]
+                names(x1)[which(names(x1) == "species")] <- "taxon"
+        }
+        if ("genus"   %in% names(x1)) {
+                x1 <- x1[!is.na(genus)]
+                names(x1)[which(names(x1) == "genus")] <- "taxon"
+        }
+        if ("family"  %in% names(x1)) {
+                x1 <- x1[!is.na(family)]
+                names(x1)[which(names(x1) == "family")] <- "taxon"
+        }
 
         #- Make sure that each taxon only occurs once per sample. This might be violated
         #- when a coarse taxonomic resolution is used and several taxa from this group were
         #- observed.
-        if ("species" %in% names(x1)) {
-                x1 <- x1[, abundance := sum(abundance),by = c("gr_sample_id", "species")]
-        }
-        if ("genus"   %in% names(x1)) {
-                x1 <- x1[, abundance := sum(abundance),by = c("gr_sample_id", "genus")]
-        }
-        if ("family"  %in% names(x1)){
-                x1 <- x1[, abundance := sum(abundance), by = c("gr_sample_id", "family")]
-        }
+        x1 <- x1[, abundance := sum(abundance), by = c("gr_sample_id", "taxon")]
+        x1 <- unique(x1, by = c("gr_sample_id", "taxon"))
+
 
         if (abundance.to.pa){
                 #- Set all abundance to 1.
-                x1 <- x1[, abundance := 1]
+                x1 <- x1[, abundance2 := 1]
         }
         x1 <- unique(x1, by = c("gr_sample_id", taxon))
         #- Drop rows where the taxonomic level is coarser then what is required by the
@@ -44,7 +52,7 @@ ct_prepare_data1 <- function(data, taxon, abundance.to.pa = TRUE, typologies) {
         #- Turn to wide format with one column for each taxon and one row per site.
         x1 <- pivot_wider(x1, id_cols = append(c("gr_sample_id"), typologies),
                      names_from = all_of(taxon),
-                     values_from = abundance,
+                     values_from = abundance2,
                      values_fill = 0)
         #- Turn to data.table.
         x1 <- setDT(x1)
