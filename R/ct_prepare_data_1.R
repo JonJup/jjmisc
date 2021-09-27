913eb68212cf92447cc2a4ee0611e0447e75dcaa
Jonathan Jupke <jonathanfrederik@aol.com>
2021-09-27 15:08:00 +0200

    mostly remove data.table from ct_prepare_data_1

--- R/ct_prepare_data_1.R
+++ R/ct_prepare_data_1.R
@@ -11,52 +11,41 @@
 #'
 ct_prepare_data1 <- function(data, taxon, typologies) {
 
+        data <- data.frame(data)
+
         #- Check inputs
-        if (!"data.table" %in% class(data)) stop("data must be a data.table")
+        if (!"data.frame" %in% class(data)) stop("data must be a data.table")
         if (!"character" %in% class(taxon)) stop("taxon must be a character")
         if (!"character" %in% class(typologies)) stop("typologies must be a character vector")
 
         #- Character vector with column names to keep
-        col.vec <- append(c("gr_sample_id", taxon, "abundance"), typologies)
+        #col.vec <- append(c("gr_sample_id", taxon, "abundance"), typologies)
+        col.vec <- c("gr_sample_id", taxon, "abundance", typologies)
         #- Subset data to columns from col.vec
-        x1 <- data[, .SD, .SDcols = col.vec]
+        #x1 <- data[, .SD, .SDcols = col.vec]
+        x1 <- data[, col.vec]
         #- remove any NA in taxon column
-        if ("species" %in% names(x1)) {
-                x1 <- x1[!is.na(species)]
-                names(x1)[which(names(x1) == "species")] <- "taxon"
-        }
-        if ("genus"   %in% names(x1)) {
-                x1 <- x1[!is.na(genus)]
-                names(x1)[which(names(x1) == "genus")] <- "taxon"
-        }
-        if ("family"  %in% names(x1)) {
-                x1 <- x1[!is.na(family)]
-                names(x1)[which(names(x1) == "family")] <- "taxon"
-        }
-
+        names(x1)[which(names(x1) %in% c("species", "genus", "family"))] <- "taxon"
+        x1 <- x1[which(!is.na(x1$taxon)), ]
         #- Make sure that each taxon only occurs once per sample. This might be violated
         #- when a coarse taxonomic resolution is used and several taxa from this group were
         #- observed.
-        setDT(x1)
-        # x1 <- x1[, abundance := sum(abundance), by = c("gr_sample_id", "taxon")]
-        x1 <- unique(x1, by = c("gr_sample_id", "taxon"))
+        x1 <- dplyr::distinct(dplyr::group_by(x1, gr_sample_id), taxon,  .keep_all = T)
         x1$abundance = 1
-
-        #- Drop rows where the taxonomic level is coarser then what is required by the
-        #- taxon argument.
-        x1 <- x1[!is.na(taxon)]
         #- Turn to wide format with one column for each taxon and one row per site.
-        x1 <- tidyr::pivot_wider(x1, id_cols = append(c("gr_sample_id"), typologies),
-                     names_from = taxon,
-                     values_from = abundance,
-                     values_fill = 0)
-        #- Turn to data.table.
-        x1 <- setDT(x1)
+        x1 <-
+                tidyr::pivot_wider(
+                        x1,
+                        id_cols = append(c("gr_sample_id"), typologies),
+                        names_from = taxon,
+                        values_from = abundance,
+                        values_fill = 0
+                )
         #- Turn NAs to 0 i.e. absence.
         x2 <- jjmisc::setNAcols(x1)
-        x1 <- setnafill(x = x1, type = "const", cols = x2, fill = 0)
+        x1 <- data.table::setnafill(x = x1, type = "const", cols = x2, fill = 0)
         #- remove columns of taxa that are always absent.
-        rm.col <- names(which(colSums(x1[,x2,with=F]) == 0))
+        rm.col <- names(which(colSums(x1[,x2]) == 0))
         if (length(rm.col) > 0) {
                 x1[, (rm.col) := NULL]
         }
