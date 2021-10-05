c9a721eb40a4e09d1377c66d8360b4b69f428a65
Jonathan Jupke <jonathanfrederik@aol.com>
2021-10-05 09:13:46 +0200

    fix add_typologies

--- R/add_typology.R
+++ R/add_typology.R
@@ -7,43 +7,34 @@
 #'
 #' @examples
 #'
-add_typologies <- function(data, debug = F){
+add_typologies <- function(data){
 
-                if(debug)
-                        print("start")
-                sites <-
-                        unique(data, by = "site_id") |>
-                        sf::st_as_sf(coords = c("x.coord", "y.coord"), crs = data4$EPSG[1]) |>
-                        dplyr::select(site_id) |>
-                        sf::st_transform(crs = sf::st_crs(typologies))
+        sites <-
+                unique(data, by = "site_id") |>
+                sf::st_as_sf(coords = c("x.coord", "y.coord"), crs = data4$EPSG[1]) |>
+                dplyr::select(site_id) |>
+                sf::st_transform(crs = sf::st_crs(typologies))
 
-                if(debug)
-                        print("sites")
+        nn <- sf::st_nearest_feature(sites, typologies)
+        nn <- typologies[nn,]
+        distances <- sf::st_distance(sites,
+                                     y = nn,
+                                     by_element = TRUE)
 
-                nn <- sf::st_nearest_feature(sites, typologies)
-                nn <- typologies[nn,]
-
-                if(debug)
-                        print("nn")
-                distances <- sf::st_distance(sites,
-                                             y = nn,
-                                             by_element = TRUE)
-
-                sites <- dplyr::mutate(sites,
-                                       distance = as.numeric(distances),
-                                       brt12    = nn$brt12,
-                                       ife      = nn$illies,
-                                       bgr      = nn$bgr,
-                                       brtXife  = nn$brtXife,
-                                       brtXbgr  = nn$brtXbgr,
-                                       least.impacted = nn$least.impacted
-                )
-                sites <- sf::st_drop_geometry(sites)
-                if(debug) print(join)
-                #- join sites with data
-                data.out <- dplyr::left_join(data,
-                                             sites,
-                                             by = "site_id")
-                data.table::setDT(data.out)
-                return(data.out)
+        sites <- dplyr::mutate(sites,
+                               distance = as.numeric(distances),
+                               brt12    = nn$brt12,
+                               ife      = nn$illies,
+                               bgr      = nn$bgr,
+                               brtXife  = nn$brtXife,
+                               brtXbgr  = nn$brtXbgr,
+                               least.impacted = nn$least.impacted
+        )
+        sites <- sf::st_drop_geometry(sites)
+        #- join sites with data
+        data.out <- dplyr::left_join(data,
+                                     sites,
+                                     by = "site_id")
+        data.table::setDT(data.out)
+        return(data.out)
 }
